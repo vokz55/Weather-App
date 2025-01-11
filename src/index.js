8ce4b3b8c2f07cb7f7413b5e39c57bf49b5bbb22
Erikh Hakobyan <vokz55@yandex.ru>
2025-01-11 15:16:47 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+var requestOptions = {
+    method: "get",
+    headers: myHeaders,
+    redirect: "follow",
+};
+
+fetch("https://v1.nocodeapi.com/erikh/ow/EykGLdqtxOoJIExR/byCityName?q=Boston", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
