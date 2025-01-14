2b4efd5213f49fc5991ed5274534b93111703e4c
Vlada Sagermanova <sagermanovavl@gmail.com>
2025-01-14 19:38:41 +0300

    Готовая вёрстка

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import './style/global.css'
 import './style/style.css'
+
+
 
 const apiKey = '1cee9811db45c4eb5dd64ff177e06d1a';
 const searchBtn = document.getElementById('searchBtn');
@@ -15,25 +18,28 @@ searchBtn.addEventListener('click', () => {
     }
 });
 
-async function fetchWeather(city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ru&appid=${apiKey}`;
-    
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Город не найден');
-        }
-        const data = await response.json();
-        displayWeather(data);
-    } catch (error) {
-        cityNameEl.textContent = 'Ошибка';
-        temperatureEl.textContent = '';
-        descriptionEl.textContent = error.message;
-    }
+function fetchWeather(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    return fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Город не найден');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            cityNameEl.textContent = 'Ошибка';
+            temperatureEl.textContent = '';
+            descriptionEl.textContent = error.message;
+        });
 }
 
 function displayWeather(data) {
-    cityNameEl.textContent = `Город: ${data.name}`;
-    temperatureEl.textContent = `Температура: ${data.main.temp}°C`;
-    descriptionEl.textContent = `Описание: ${data.weather[0].description}`;
+    cityNameEl.textContent = `City: ${data.name}`;
+    temperatureEl.textContent = `Temperature: ${data.main.temp}°C`;
+    descriptionEl.textContent = `Description: ${data.weather[0].description}`;
 }
